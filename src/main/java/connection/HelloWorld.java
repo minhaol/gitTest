2da0d65a1f4c72a8ee213ba57e0bc3350b3a2b23
Minhao Liang <350687497@qq.com>
2022-03-12 18:46:54 +0800

    first test

---
+++ src/main/java/connection/HelloWorld.java
@@ -0,0 +1,11 @@
+package connection;
+
+/**
+ * @author minhao, Liang
+ * @create 2022 - 03 - 12 18:16
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
